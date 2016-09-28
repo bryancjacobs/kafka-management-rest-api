a0eb6149385a53f68e057580fa5767e41a6147bb
bryan.jacobs <bryan.jacobs@martizcx.com>
2016-09-28 15:01:56 -0600

    generation of scalatra project structure

---
+++ project/build.scala
@@ -0,0 +1,36 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import com.earldouglas.xwp.JettyPlugin
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+
+object KafkaManagementRestApiBuild extends Build {
+  val Organization = "com.maritzcx"
+  val Name = "kafka-management-rest-api"
+  val Version = "1.0"
+  val ScalaVersion = "2.11.8"
+  val ScalatraVersion = "2.4.1"
+
+  lazy val project = Project (
+    "kafka-management-rest-api",
+    file("."),
+    settings = ScalatraPlugin.scalatraSettings ++ scalateSettings ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
+        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
+      )
+    )
+  ).enablePlugins(JettyPlugin)
+}
---
+++ src/main/scala/com/maritzcx/kafka/mgmt/rest/api/KafkaScalatraServlet.scala
@@ -0,0 +1,16 @@
+package com.maritzcx.kafka.mgmt.rest.api
+
+import org.scalatra._
+
+class KafkaScalatraServlet extends KafkaManagementRestApiStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
