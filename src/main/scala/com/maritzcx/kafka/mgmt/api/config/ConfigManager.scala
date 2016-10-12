93657b6ad68c2c2f077ad1cb471e17f8fda24fa0
bryan.jacobs <bryan.jacobs@martizcx.com>
2016-10-12 14:32:54 -0600

    update config manager to support a config file

--- src/main/scala/com/maritzcx/kafka/mgmt/api/config/ConfigManager.scala
+++ src/main/scala/com/maritzcx/kafka/mgmt/api/config/ConfigManager.scala
@@ -10,17 +10,46 @@ object ConfigManager {
 
   val LOG = LoggerFactory.getLogger(this.getClass)
 
-  val env = System.getProperty("env")
+  val env = Option.apply(System.getProperty("env"))
 
-  LOG.info(s"Application Configured with: env=$env")
+  LOG.info(s"env=${env}")
 
-  val config = if(env == null || env.isEmpty) {
-    LOG.info(s"Loading: application.conf")
-    ConfigFactory.load("application.conf")
-  }
-  else {
-    LOG.info(s"Loading: application-${env}.conf")
-    ConfigFactory.load(s"application-${env}.conf")
+  val configFile = Option.apply(System.getProperty("config.file"))
+
+  LOG.info(s"configFile=${configFile}")
+
+  // if we have a config.file specified favor that
+  // otherwise fall back to the environment variable
+  val config = configFile match {
+
+    case Some(file) => {
+
+      LOG.info(s"Application Configured with: config.file=${configFile.get}")
+
+      ConfigFactory.load()
+
+    }
+    case None => {
+
+      LOG.info(s"Application Configured with: env=${env}")
+
+      env match {
+
+        case Some(env) =>{
+
+          LOG.info(s"Loading: application-${env}.conf")
+
+          ConfigFactory.load(s"application-${env}.conf")
+
+        }
+        case None => {
+          LOG.info(s"Loading: application.conf")
+
+          ConfigFactory.load("application.conf")
+
+        }
+      }
+    }
   }
 
   def getZkHost(): String = {
